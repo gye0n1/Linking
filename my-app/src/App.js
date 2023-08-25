751d1824aaf430da5892b8992efcb3872cce1f31
gueyeon <1230rbdus@gmail.com>
2023-08-25 21:03:16 +0900

    9:03

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <h1>하이</h1>;
+  return (
+    <div>
+      <p class="title">LiNking</p>
+      <div class="bar">
+        <div class="menu1"></div>
+        <div class="menu2"></div>
+        <div class="menu3"></div>
+        <p class="채용정보">채용정보</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
