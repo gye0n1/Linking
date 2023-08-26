676621e5740c16f24b29fbe37f141f4521a6020a
gye0n1 <1230rbdus@gmail.com>
2023-08-27 07:30:54 +0900

    8/27 am 07:30

--- my-app/src/Test.js
+++ my-app/src/Test.js
@@ -1,31 +1,39 @@
 import "./Test.css";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Test() {
   const navigate = useNavigate();
+  const [optionClicked, setOptionClicked] = useState({});
 
   const goToresult = () => {
-    navigate("/Testresult"); // "/Testresult" 경로로 이동
+    navigate("/Testresult");
+  };
+
+  const handleOptionClick = (questionId, optionId) => {
+    setOptionClicked((prevState) => ({
+      ...prevState,
+      [questionId]: optionId,
+    }));
   };
 
   return (
     <div>
-      <span class="title">LiNking</span>
-      <div class="bar">
-        <div class="menu1"></div>
-        <div class="menu2"></div>
-        <div class="menu3"></div>
-        <span class="채용공고">채용공고</span>
-        <div class="line1"></div>
-        <span class="직무적성테스트">직무적성테스트</span>
-        <div class="line2"></div>
-        <span class="직업찾기">직업찾기</span>
-        <span class=""></span>
-        <div class="line3"></div>
-        <span class="채용설명회">채용설명회</span>
-        <div class="line6"></div>
-        <span class="center">고객센터</span>
+      <span className="title">LiNking</span>
+      <div className="bar">
+        <div className="menu1"></div>
+        <div className="menu2"></div>
+        <div className="menu3"></div>
+        <span className="채용공고">채용공고</span>
+        <div className="line1"></div>
+        <span className="직무적성테스트">직무적성테스트</span>
+        <div className="line2"></div>
+        <span className="직업찾기">직업찾기</span>
+        <span className=""></span>
+        <div className="line3"></div>
+        <span className="채용설명회">채용설명회</span>
+        <div className="line6"></div>
+        <span className="center">고객센터</span>
       </div>
 
       <span className="testtitle">직무 적성 테스트</span>
@@ -42,45 +50,116 @@ function Test() {
             height="83"
             rx="10"
             fill="#427BE1"
-            fill-opacity="0.92"
+            fillOpacity="0.92"
           />
         </svg>
+
+        {/* Question 1 */}
         <div className="question-container">
           <span className="q1">
             1. 여가 시간에 어떤 종류의 활동을 가장 즐기시나요?
           </span>
-          <span className="q1a1">a) 데이터 분석과 문제 해결</span>
-          <span className="q1a2">b) 예술과 창작적인 활동</span>
-          <span className="q1a3">c) 사람들과 협력하는 것</span>
-          <span className="q1a4">d) 물건을 만드는 것</span>
+          <span
+            className={optionClicked["q1"] === "q1a1" ? "q1a1 clicked" : "q1a1"}
+            onClick={() => handleOptionClick("q1", "q1a1")}>
+            a) 데이터 분석과 문제 해결
+          </span>
+          <span
+            className={optionClicked["q1"] === "q1a2" ? "q1a2 clicked" : "q1a2"}
+            onClick={() => handleOptionClick("q1", "q1a2")}>
+            b) 예술과 창작적인 활동
+          </span>
+          <span
+            className={optionClicked["q1"] === "q1a3" ? "q1a3 clicked" : "q1a3"}
+            onClick={() => handleOptionClick("q1", "q1a3")}>
+            c) 사람들과 협력하는 것
+          </span>
+          <span
+            className={optionClicked["q1"] === "q1a4" ? "q1a4 clicked" : "q1a4"}
+            onClick={() => handleOptionClick("q1", "q1a4")}>
+            d) 물건을 만드는 것
+          </span>
         </div>
 
+        {/* Question 2 */}
         <div className="question-container">
           <span className="q2">
             2. 학교에서 가장 흥미로웠거나 능력을 발휘했던 과목은 무엇인가요?
           </span>
-          <span className="q2a1">a) 수학, 과학, 컴퓨터 과학</span>
-          <span className="q2a2">b) 언어, 문학, 예술</span>
-          <span className="q2a3">c) 사회과학, 심리학, 사회복지</span>
-          <span className="q2a4">d) 공학, 건축, 공예</span>
+          <span
+            className={optionClicked["q2"] === "q2a1" ? "q2a1 clicked" : "q2a1"}
+            onClick={() => handleOptionClick("q2", "q2a1")}>
+            a) 수학, 과학, 컴퓨터 과학
+          </span>
+          <span
+            className={optionClicked["q2"] === "q2a2" ? "q2a2 clicked" : "q2a2"}
+            onClick={() => handleOptionClick("q2", "q2a2")}>
+            b) 언어, 문학, 예술
+          </span>
+          <span
+            className={optionClicked["q2"] === "q2a3" ? "q2a3 clicked" : "q2a3"}
+            onClick={() => handleOptionClick("q2", "q2a3")}>
+            c) 사회과학, 심리학, 사회복지
+          </span>
+          <span
+            className={optionClicked["q2"] === "q2a4" ? "q2a4 clicked" : "q2a4"}
+            onClick={() => handleOptionClick("q2", "q2a4")}>
+            d) 공학, 건축, 공예
+          </span>
         </div>
 
+        {/* Question 3 */}
         <div className="question-container">
           <span className="q3">3. 어떤 작업 환경을 선호하시나요?</span>
-          <span className="q3a1">a) 고요한 사무실에서 개인적인 작업</span>
-          <span className="q3a2">b) 창의적인 분위기의 아트 스튜디오</span>
-          <span className="q3a3">c) 다양한 사람들과 함께하는 협업 공간</span>
-          <span className="q3a4">d) 실제 물건을 다루는 작업장</span>
+          <span
+            className={optionClicked["q3"] === "q3a1" ? "q3a1 clicked" : "q3a1"}
+            onClick={() => handleOptionClick("q3", "q3a1")}>
+            a) 고요한 사무실에서 개인적인 작업
+          </span>
+          <span
+            className={optionClicked["q3"] === "q3a2" ? "q3a2 clicked" : "q3a2"}
+            onClick={() => handleOptionClick("q3", "q3a2")}>
+            b) 창의적인 분위기의 아트 스튜디오
+          </span>
+          <span
+            className={optionClicked["q3"] === "q3a3" ? "q3a3 clicked" : "q3a3"}
+            onClick={() => handleOptionClick("q3", "q3a3")}>
+            c) 다양한 사람들과 함께하는 협업 공간
+          </span>
+          <span
+            className={optionClicked["q3"] === "q3a4" ? "q3a4 clicked" : "q3a4"}
+            onClick={() => handleOptionClick("q3", "q3a4")}>
+            d) 실제 물건을 다루는 작업장
+          </span>
         </div>
 
-
+        {/* Question 4 */}
         <div className="question-container">
-        <span className="q4">4. 팀 프로젝트에서 어떤 역할을 선호하시나요?</span>
-        <span className="q4a1">a) 전문적인 지식과 분석적 사고로 기여</span>
-        <span className="q4a2">b) 새로운 아이디어와 창의성으로 기여</span>
-        <span className="q4a3">c) 타인과의 조화로운 협력과 소통으로 기여</span>
-        <span className="q4a4">d) 실용적인 기술과 노력으로 기여</span>
+          <span className="q4">
+            4. 팀 프로젝트에서 어떤 역할을 선호하시나요?
+          </span>
+          <span
+            className={optionClicked["q4"] === "q4a1" ? "q4a1 clicked" : "q4a1"}
+            onClick={() => handleOptionClick("q4", "q4a1")}>
+            a) 전문적인 지식과 분석적 사고로 기여
+          </span>
+          <span
+            className={optionClicked["q4"] === "q4a2" ? "q4a2 clicked" : "q4a2"}
+            onClick={() => handleOptionClick("q4", "q4a2")}>
+            b) 새로운 아이디어와 창의성으로 기여
+          </span>
+          <span
+            className={optionClicked["q4"] === "q4a3" ? "q4a3 clicked" : "q4a3"}
+            onClick={() => handleOptionClick("q4", "q4a3")}>
+            c) 타인과의 조화로운 협력과 소통으로 기여
+          </span>
+          <span
+            className={optionClicked["q4"] === "q4a4" ? "q4a4 clicked" : "q4a4"}
+            onClick={() => handleOptionClick("q4", "q4a4")}>
+            d) 실용적인 기술과 노력으로 기여
+          </span>
         </div>
+
         <span className="butcon" onClick={goToresult}>
           테스트 결과보기
         </span>
@@ -88,4 +167,5 @@ function Test() {
     </div>
   );
 }
+
 export default Test;
--- my-app/src/Testresult.js
+++ my-app/src/Testresult.js
@@ -10,30 +10,50 @@ function Testresult() {
         {/* <img
           src="/Users/dgsw8th42/Desktop/React/Frontend/Linking/my-app/src/result.png"
           alt="이미지 없음"></img> */}
-        <div className="box1"></div>
-        <div className="box2"></div>
-        <div className="box3"></div>
+        <div className="box1">
+          <span className="com1-1">Co</span>
+          <span className="com1-2">nnection</span>
+          <span className="com2">Workin'</span>
+          <span className="com3-1">Bh</span>
+          <span className="com3-2">aap</span>
+          <span className="com3-3">y</span>
+        </div>
+        <div className="box2">
+          <span className="edu">
+            경영학
+            <br />
+            마케팅
+            <br />
+            재무관리
+            <br />
+            조직이론
+            <br />
+            전략경영
+            <br />
+            이슈매니지먼트
+          </span>
+        </div>
+        <div className="box3">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlWH7j0YCgLB3lRBZngvFmgpZITRCdnTti9A&usqp=CAU"
+            alt="인프런"
+            className="inflearn"></img>
+
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVoF6u12Xu9TCg7PTfuuRkhbckwWRDSrqIHw&usqp=CAU"
+            alt="카테노이드"
+            className="catenoid"></img>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWZ2-BgPx6TqWX1YEDTF3E5wph9Qd9FjfEAg&usqp=CAU"
+            alt="클래스101"
+            className="class101"></img>
+        </div>
         <div className="space1"></div>
         <div className="space2"></div>
         <div className="space3"></div>
         <span className="con1">관련회사</span>
         <span className="con2">공부과목</span>
         <span className="con3">교육프로그램</span>
-        <span className="edu">
-          경영학
-          <br />
-          마케팅
-          <br />
-          재무관리
-          <br />
-          조직이론
-          <br />
-          전략경영
-          <br />
-          이슈매니지먼트
-        </span>
-        <span className="company">예시 1<br/><br/>예시2<br/><br/>예시3</span>
-        <span className="edupro">예시 1<br/><br/>예시2<br/><br/>예시3</span>
       </div>
     </div>
   );
